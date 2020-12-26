668e7e363aa13b4e9f1861909f4e0bbf3e593a48
always0ne <always0ne.dev@gmail.com>
2020-12-26 22:21:22 +0900

    feat: jwtAuth token exception handling is needed

--- src/main/java/com/example/webflux/api/community/controller/PostController.java
+++ src/main/java/com/example/webflux/api/community/controller/PostController.java
@@ -1,4 +1,31 @@
 package com.example.webflux.api.community.controller;
 
+import com.example.webflux.security.JwtTokenProvider;
+import com.example.webflux.security.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+
+import static com.example.webflux.security.JwtTokenProvider.getUserIdFromAuth;
+
+@RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @GetMapping("/test")
+    String testCont() {
+        return jwtTokenProvider.createAccessToken("testUser", Collections.singletonList(UserRole.ROLE_USER));
+    }
+
+    @PostMapping("/test")
+    Mono<String> testCont2() {
+        return getUserIdFromAuth();
+    }
+
 }
